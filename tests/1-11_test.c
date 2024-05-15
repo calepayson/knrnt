f453d2daf5625ab2a250caafed901b0ccf75f3ae
Cale Smith <calepayson@mac.com>
2024-05-15 09:36:04 -0700

    Exercise 1-11 and tests

---
+++ tests/1-11_test.c
@@ -0,0 +1,13 @@
+/* TEST 1-09 */
+
+#include <stdio.h>
+
+#include "../headers/1-10.h"
+#include "../headers/test_helpers.h"
+
+int main(void) {
+    printf("No tests for this exercise (feel free to write your own though!)\n");
+    printf("SUCCESS! Wooooo! Crushed that exercise!\n");
+
+    return 0;
+}
